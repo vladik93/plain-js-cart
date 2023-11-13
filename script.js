6ac94baabdf20f2d5a58f1e489c784b13fb5d5f5
Vladi <39379713+vladik93@users.noreply.github.com>
2023-11-13 11:17:46 +0200

    cart UI updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const productsEl = document.getElementById("products");
 const cartItemsEl = document.getElementById("cart-items");
+const body = document.body;
 
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 
@@ -72,35 +73,79 @@ const updateCartAmount = () => {
 };
 
 const generateCartItems = () => {
-  cartItemsEl.innerHTML += cart.map((item) => {
-    return `
+  return cart
+    .map((item) => {
+      console.log("generateCartItems ===>");
+      console.log(item);
+      return `
         <div class="cart-item">
-                <div class="cart-item-image-cont">
                   <img
-                    src="./assets/products/headphones.jpg"
+                    src="./assets/products/${item.image}"
                     class="image cart-item-image"
                   />
-                </div>
                 <div class="cart-item-content-container">
                   <div class="cart-item-content">
-                    <h2 class="cart-item-title">Earphones</h2>
-                    <h2 class="cart-item-title">$2.50</h2>
-                    <p class="cart-item-description">Item description text here</p>
+                    <h2 class="cart-item-title">${item.title}</h2>
+                    <h2 class="cart-item-title">$${item.price}</h2>
+                    <p class="cart-item-description">${item.description}</p>
                   </div>
                   <div class="cart-item-actions">
                     <button class="cart-item-increment button">
                       <img src="./assets/icons/minus.svg" class="icon-sm" />
                     </button>
-                    <span>0</span>
+                    <span>${item.amount}</span>
                     <button class="cart-item-increment button">
                       <img src="./assets/icons/plus.svg" class="icon-sm" />
                     </button>
                   </div>
                 </div>
               </div>`;
-  });
+    })
+    .join("");
+};
+
+const generateCartModal = () => {
+  return (body.innerHTML += `
+  <div id="cart-modal" class="cart-modal">
+    <div class="cart-modal-header">
+      <h2>My Cart</h2>
+      <button class="cart-modal-close" onclick="closeCartModal()">
+        <img src="./assets/icons/close.svg" class="icon-sm" />
+      </button>
+    </div>
+    <div id="cart-items" class="cart-items">
+      ${generateCartItems()}
+    </div>
+    <div class="cart-modal-footer">
+      <div class="cart-details">
+        <div class="cart-detail">
+          <span>Subtotal</span>
+          <span>$55.21</span>
+        </div>
+        <div class="cart-detail">
+          <span>Tax & Fees</span>
+          <span>$3.00</span>
+        </div>
+        <div class="cart-detail">
+          <span>Delivery</span>
+          <span>Free</span>
+        </div>
+      </div>
+      <div class="cart-checkout-container">
+        <button class="button cart-checkout-button">CHECKOUT</button>
+      </div>
+    
+    
+    </div>
+</div>`);
+};
+
+const closeCartModal = () => {
+  const cartModalEl = document.getElementById("cart-modal");
+
+  cartModalEl.remove();
 };
 
 generateProducts();
 updateCartAmount();
-generateCartItems();
+generateCartModal();
